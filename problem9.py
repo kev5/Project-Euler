12d1c776bd1f806d0b07bbb2079b853fbf8e1c27
Keval Khara <kevalkhara95@gmail.com>
2017-10-24 00:04:41 -0400

    Update problem9.py

--- problem9.py
+++ problem9.py
@@ -3,6 +3,8 @@
 import math
 sum = 1000
 
+# We know the fact that a<b<c and thus a<sum/3 and a<b<sum/2
+
 for a in range(1,math.floor(sum/3)):
 	for b in range(a,math.floor(sum/2)):
 		c = sum - a - b
