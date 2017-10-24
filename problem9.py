a4d7375af76da0126761fdf675b4d68eeebd314b
Keval Khara <kevalkhara95@gmail.com>
2017-10-24 00:02:07 -0400

    Corrected string and int error in print function

--- problem9.py
+++ problem9.py
@@ -1,10 +1,11 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+import math
 sum = 1000
 
-for a in range(1,sum/3):
-	for b in range(a,sum/2):
+for a in range(1,math.floor(sum/3)):
+	for b in range(a,math.floor(sum/2)):
 		c = sum - a - b
 
 		if(a*a + b*b == c*c):
-			print(a + "*" + b + "*" + c + "=" + a*b*c)
+			print(str(a) + "*" + str(b) + "*" + str(c) + "=" + str(a*b*c))
