8b86f7e5f117f75fa58c2596e0eee8ee771c2dcc
Keval Khara <kevalkhara95@gmail.com>
2017-10-31 17:26:20 -0400

    Highly divisible triangular number

--- problem012.py
+++ problem012.py
@@ -1,2 +1,32 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+def num_divisors(n):
+    if n % 2 == 0: 
+    	n = n/2
+    divisors = 1
+    count = 0
+    while n % 2 == 0:
+        count += 1
+        n = n/2
+    divisors = divisors * (count + 1)
+    p = 3
+    while n != 1:
+        count = 0
+        while n % p == 0:
+            count += 1
+            n = n/p
+        divisors = divisors * (count + 1)
+        p += 2
+    return divisors
+ 
+def find_triangular_index(factor_limit):
+    n = 1
+    lnum, rnum = num_divisors(n), num_divisors(n+1)
+    while lnum * rnum < 500:
+        n += 1
+        lnum, rnum = rnum, num_divisors(n+1)
+    return n
+ 
+index = find_triangular_index(500)
+triangle = (index * (index + 1)) / 2
+print(triangle)
