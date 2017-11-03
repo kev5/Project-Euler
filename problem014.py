5fd3b7a866391dc53ae5276463526d747c602035
Keval Khara <kevalkhara95@gmail.com>
2017-11-03 00:02:42 -0400

    Longest Collatz Sequence

--- problem014.py
+++ problem014.py
@@ -7,7 +7,7 @@ max_length = [1,1]
  
 for i in range(1,1000000):
     n,s = i,0
-    TO_ADD = [] # collatz_length not yet known
+    TO_ADD = []       # collatz_length not yet known
     while n > limit - 1 or collatz_length[int(n)] < 1:
         TO_ADD.append(n)
         if n % 2 == 0: n = n/2
@@ -22,28 +22,3 @@ for i in range(1,1000000):
             collatz_length[int(m)] = new_length
             if new_length > max_length[1]: max_length = [i,new_length]
 print(max_length[0])
-
-# def k(upto):
-#     def collatz(n):
-#         if n < upto and lst[int(n)] > 0:
-#             return lst[int(n)]
-#         if n % 2 == 0:
-#             val = collatz(n/2) + 1
-#         else:
-#             val = collatz((3*n + 1)/2) + 2
-#         if n < upto:
-#             lst[int(n)] = val
-#         return val
-
-#     lst = [0]*upto
-#     lst[1] = 1
-#     lst[0] = 1
-#     for i in range(upto):
-#         collatz(i)
-#     return max(lst)
-
-# def main():
-# 	print(k(1000000))
-
-# if __name__ == '__main__': 
-# 	main()
