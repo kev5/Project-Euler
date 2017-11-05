b5a0044d3ee7c55c1342ddd25e07ad17b218cb00
Keval Khara <kevalkhara95@gmail.com>
2017-11-05 10:29:46 -0500

    Lattice Paths

--- problem015.py
+++ problem015.py
@@ -1,1 +1,11 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
+
+def route_num(cube_size):
+    L = [1] * cube_size
+    for i in range(cube_size):
+        for j in range(i):
+            L[j] = L[j]+L[j-1]
+        L[i] = 2 * L[i - 1]
+    return L[cube_size - 1]
+ 
+print(route_num(20))
