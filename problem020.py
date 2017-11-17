9eeac5ae25e9267b26bf7ca2ea9a2b49ccdb6403
Keval Khara <kevalkhara95@gmail.com>
2017-11-17 12:49:23 -0500

    Update problem020.py

--- problem020.py
+++ problem020.py
@@ -1,1 +1,14 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
+
+
+def factorial(n):
+    if n == 0: return 1
+    else: return n * factorial(n - 1)
+ 
+
+def sum_digits(n):
+    return sum([int(i) for i in str(n)])
+ 
+sum = sum_digits(factorial(100))
+
+print(sum)
