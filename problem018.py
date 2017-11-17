4a62552297a6206fb04c0cd9569445d36e2c6f9c
Keval Khara <kevalkhara95@gmail.com>
2017-11-16 23:58:28 -0500

    Update problem018.py

--- problem018.py
+++ problem018.py
@@ -1,2 +1,7 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+def compute():
+	for i in reversed(range(len(triangle) - 1)):
+		for j in range(len(triangle[i])):
+			triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
+	return str(triangle[0][0])
