157f917e958e2879031c14e55ce2210064976e1e
Keval Khara <kevalkhara95@gmail.com>
2017-11-17 12:35:18 -0500

    Update problem019.py

--- problem019.py
+++ problem019.py
@@ -1,2 +1,16 @@
 # Copyright 22017 Keval Khara kevalk@bu.edu
 
+'''
+Using the Gaussian Algorithm to caluclate the day of the week:
+  
+   w = (d+floor(2.6*m-0.2)+y+floor(y/4)+floor(c/4)-2*c) mod 7
+ 
+    Y = year - 1 for January or February
+    Y = year for other months
+    d = day (1 to 31)
+    m = shifted month (March = 1, February = 12)
+    y = last two digits of Y
+    c = first two digits of Y
+    w = day of week (Sunday = 0, Saturday = 6)
+'''
+
