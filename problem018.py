35b0c7c9ccec92af27bd14ba37e80dcd7dfefd68
Keval Khara <kevalkhara95@gmail.com>
2017-11-16 23:59:45 -0500

    Update problem018.py

--- problem018.py
+++ problem018.py
@@ -1,6 +1,6 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
-def compute():
+def maxsum():
 	for i in reversed(range(len(triangle) - 1)):
 		for j in range(len(triangle[i])):
 			triangle[i][j] += max(triangle[i + 1][j], triangle[i + 1][j + 1])
