05e8c79a3a4e6f94036d8dc167d75ad8580e4460
Keval Khara <kevalkhara95@gmail.com>
2017-11-25 19:27:31 -0500

    Amicable numbers

--- problem021.py
+++ problem021.py
@@ -1,14 +1,15 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
-divisorsum = [0] * 10000
-	for i in range(1, len(divisorsum)):
-		for j in range(i * 2, len(divisorsum), i):
-			divisorsum[j] += i
-	
-	# Find all amicable pairs within range
-	ans = 0
-	for i in range(1, len(divisorsum)):
-		j = divisorsum[i]
-		if j != i and j < len(divisorsum) and divisorsum[j] == i:
-			ans += i
-	return str(ans)
+# Calculate the sum of proper divisors for each number
+limit = [0] * 10000
+for i in range(1, len(limit)):
+	for j in range(i * 2, len(limit), i):
+		limit[j] += i
+
+# Find all amicable pairs within specified range
+ans = 0
+for i in range(1, len(limit)):
+	j = limit[i]
+	if j != i and j < len(limit) and limit[j] == i:
+		ans += i
+print(ans)
