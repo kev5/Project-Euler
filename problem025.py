37724ec736d14d05d8e2c13839da12d3bf49e041
Keval Khara <kevalkhara95@gmail.com>
2017-11-26 21:26:14 -0500

    1000-digit Fibonacci Number

--- problem025.py
+++ problem025.py
@@ -1,1 +1,14 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
+
+fib = [0,1]
+
+i = 2
+
+while True:
+	fib_new = fib[i-1]+fib[i-2]
+	fib.append(fib_new)
+
+	if fib_new > (10**999):
+		print(i)
+		break
+	i = i+1
