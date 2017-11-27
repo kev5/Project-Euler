1f1c5553398c50018440bdf55bc789b9c2d36954
Keval Khara <kevalkhara95@gmail.com>
2017-11-26 19:27:36 -0500

    Lexicographic permutations

--- problem024.py
+++ problem024.py
@@ -1,1 +1,11 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
+
+from itertools import permutations
+
+lexi_perm = list(permutations('0123456789'))
+
+# Each permutation is in the form of tuple
+# so joining all the strings 
+ans = ''.join(lexi_perm[999999])
+
+print(ans)
