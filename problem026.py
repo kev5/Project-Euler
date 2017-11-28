6b853bb6af27a0781e7e85c5870c553c5da80af6
Keval Khara <kevalkhara95@gmail.com>
2017-11-28 14:10:41 -0500

    Update problem026.py

--- problem026.py
+++ problem026.py
@@ -1,2 +1,16 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+from itertools import count
+
+def reciprocal_cycle(n):
+	res = {}
+	x = 1
+	for d in count():	# itertools.count need not have a range
+		if x in res:
+			return d - res[x]
+		else:
+			res[x] = d
+			x = x * 10 % n
+
+ans = max(range(1,1000), key = reciprocal_cycle)
+print(ans)
