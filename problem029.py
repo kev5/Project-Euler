5374de6e9dd45099cba68cc54e1ed08fd732db0a
Keval Khara <kevalkhara95@gmail.com>
2017-12-27 13:57:50 -0500

    Distinct Powers

--- problem029.py
+++ problem029.py
@@ -1,4 +1,9 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+'''
+A set is the best way to store values when we do not want
+any duplicate values in our result
+'''
+
 result = set(a**b for a in range(2, 101) for b in range(2, 101))
 print(len(result))
