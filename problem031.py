a50d755a8e7ac78a9f3339682fb5326f6ed842ab
Keval Khara <kevalkhara95@gmail.com>
2018-01-06 21:31:05 -0500

    Coin Sums

--- problem031.py
+++ problem031.py
@@ -1,13 +1,15 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
-TOTAL = 200
+SUM = 200
 
-# At the start of each loop iteration, ways[i] is the number of ways to use
-# {any copies of the all the coin values seen before this iteration}
+# At the start of each loop iteration, ways[i] is
+# the number of ways to use any copies of all
+# the coin values seen before this iteration
 # to form an unordered sum of i
 
-ways = [1] + [0] * TOTAL
-for coin in [1, 2, 5, 10, 20, 50, 100, 200]:
+ways = [1] + [0] * SUM
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+for coin in coins:
   for i in range(len(ways) - coin):
     ways[i + coin] += ways[i]
 
