b8c61b3194c185549d040e4ac95a7c011333037c
Keval Khara <kevalkhara95@gmail.com>
2018-01-08 21:11:24 -0500

    Update problem032.py

--- problem032.py
+++ problem032.py
@@ -1,1 +1,18 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
+
+# Copyright 2017 Keval Khara kevalk@bu.edu
+
+from math import sqrt
+
+
+def has_pandigital_product(n):
+	# Find and examine all factors of n
+	for i in range(1, int(sqrt(n) + 1)):
+		if n % i == 0:
+			temp = str(n) + str(i) + str(n // i)
+			if "".join(sorted(temp)) == "123456789":
+				return True
+	return False
+
+ans = sum(i for i in range(1, 10000) if has_pandigital_product(i))
+print(ans)
