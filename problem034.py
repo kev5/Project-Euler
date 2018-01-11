dacfd4b1ed719037435946f8365ef90f366ca368
Keval Khara <kevalkhara95@gmail.com>
2018-01-11 14:11:19 -0500

    Update problem034.py

--- problem034.py
+++ problem034.py
@@ -1,1 +1,33 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
+
+# Copyright 2018 Keval Khara kevalk@bu.edu
+
+from math import factorial as fact
+
+
+f = [fact(0),
+fact(1),
+fact(2),
+fact(3),
+fact(4),
+fact(5),
+fact(6),
+fact(7),
+fact(8),
+fact(9)]
+
+
+def factorial_digits(n):
+	digit = 0
+	while n:
+		digit += f[n%10]
+		n //= 10
+	return digit
+
+ans = 0
+
+for i in range(10,1000000):
+	if factorial_digits(i) == i:
+		ans += i
+
+print(ans)
