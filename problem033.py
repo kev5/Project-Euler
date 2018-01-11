adc4e03841db77ef9b1133fa2016d7cdc33a9551
Keval Khara <kevalkhara95@gmail.com>
2018-01-11 13:51:56 -0500

    Update problem033.py

--- problem033.py
+++ problem033.py
@@ -1,1 +1,18 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
+
+# Copyright 2018 Keval Khara kevalk@bu.edu
+
+from fractions import gcd
+
+num = 1
+den = 1
+for d in range(10, 100):
+	for n in range(10, d):
+		n0 = n % 10
+		n1 = n // 10
+		d0 = d % 10
+		d1 = d // 10
+		if (n1 == d0 and n0 * d == n * d1) or (n0 == d1 and n1 * d == n * d0):
+			num *= n
+			den *= d
+print(den // gcd(num, den))
