f460b0ea93c9d35a5586a1f1d459b39273bc1a37
Keval Khara <kevalkhara95@gmail.com>
2018-01-11 18:14:04 -0500

    Finding Primes Efficiently

---
+++ 0_PrimeSieve.py
@@ -0,0 +1,25 @@
+
+
+import math
+
+
+def primeSieve(sieveSize):
+	'''Returns a list of prime numbers'''
+	sieve = [True] * sieveSize
+	# 0 and 1 are not prime
+	sieve[0] = False
+	sieve[1] = False
+
+	# Create the sieve
+	for i in range(2, int(math.sqrt(sieveSize)) + 1):
+		pointer = i * 2
+		while pointer < sieveSize:
+			sieve[pointer] = False
+			pointer += i
+
+	# Compile the list of primes
+	primes = []
+	for i in range(sieveSize):
+		if sieve[i] == True:
+			primes.append(i)
+	return primes
