eeacca8ff594eacb5dee049dba330f3898e86de5
Keval Khara <kevalkhara95@gmail.com>
2018-01-12 16:31:30 -0500

    Double-base Palindromes

--- problem036.py
+++ problem036.py
@@ -4,7 +4,7 @@ def is_db_palindrome(n):
 	s = str(n)
 	if s != s[ : : -1]:
 		return False
-	t = bin(n)[2 : ]
+	t = bin(n)[2 : ]       # Since bin(10) = 0b1010, we leave out the first 2 characters
 	if t != t[ : : -1]:
 		return False
 	return True
