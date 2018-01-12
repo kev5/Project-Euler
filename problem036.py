e82fe5b40764dd59664a81fefae6a0c7449df649
Keval Khara <kevalkhara95@gmail.com>
2018-01-12 16:29:35 -0500

    Update problem036.py

--- problem036.py
+++ problem036.py
@@ -1,2 +1,15 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
 
+# Copyright 2018 Keval Khara kevalk@bu.edu
+
+def is_db_palindrome(n):
+	s = str(n)
+	if s != s[ : : -1]:
+		return False
+	t = bin(n)[2 : ]
+	if t != t[ : : -1]:
+		return False
+	return True
+
+ans = sum(i for i in range(1000000) if is_db_palindrome(i))
+print(ans)
