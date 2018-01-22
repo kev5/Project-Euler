25e0bf1aa942faa7d3056ca7d90ab5929dfe9704
Keval Khara <kevalkhara95@gmail.com>
2018-01-22 13:00:37 -0500

    Update problem039.py

--- problem039.py
+++ problem039.py
@@ -1,1 +1,17 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
+
+from collections import Counter
+
+perimeters = []
+
+for a in range(1, 500):
+    for b in range(a, 500):
+        c = (a**2 + b**2)**0.5
+        if int(c) == c and a + b + c <= 1000:
+            perimeters.append(a+b+c)
+
+# Counting the instances of perimeters
+p = Counter(perimeters)
+
+# Printing the most repeated perimeter
+print(p.most_common(1)[0])
