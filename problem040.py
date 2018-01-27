b5be4e118b5085800663e6b354ae1bf3be785c33
Keval Khara <kevalkhara95@gmail.com>
2018-01-27 17:19:16 -0500

    Update problem040.py

--- problem040.py
+++ problem040.py
@@ -1,1 +1,9 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
+
+# Copyright 2018 Keval Khara kevalk@bu.edu
+
+s = "".join(str(i) for i in range(1, 1000000))
+ans = 1
+for i in range(7):
+	ans *= int(s[10**i - 1])
+print(ans)
