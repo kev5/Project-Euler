466afec49691de3ca02bfc70223fdd282847489b
Keval Khara <kevalkhara95@gmail.com>
2018-05-28 21:43:03 -0400

    Update problem042.py

--- problem042.py
+++ problem042.py
@@ -1,1 +1,7 @@
-# Copyright 2018 Keval Khara kevalk@bu.edu
+# Copyright 2017 Keval Khara kevalk@bu.edu
+
+from math import sqrt
+score = lambda word: sum(ord(c)-64 for c in word)
+is_tri_num = lambda t: (sqrt(1 + 8*t) - 1) / 2 % 1 == 0
+
+print(sum(is_tri_num(score(x[1:-1])) for x in open('p042_words.txt').read().split(',')))
