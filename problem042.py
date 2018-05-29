57eceaf11899d8627a5d50c034a8faf8802f3ad5
Keval Khara <kevalkhara95@gmail.com>
2018-05-28 21:45:34 -0400

    Coded triangle numbers

--- problem042.py
+++ problem042.py
@@ -1,5 +1,13 @@
 # Copyright 2017 Keval Khara kevalk@bu.edu
 
+'''
+This problem gets much easier if you first solve the equation
+t_n = n(n+1)/2
+which gives us-
+n^2 + n - 2t_n = 0
+which can be solved to find the value of n
+'''
+
 from math import sqrt
 score = lambda word: sum(ord(c)-64 for c in word)
 is_tri_num = lambda t: (sqrt(1 + 8*t) - 1) / 2 % 1 == 0
