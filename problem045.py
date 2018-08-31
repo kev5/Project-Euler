ea45e01c5054dc871fb7dbe62948b35a2c6e348f
Keval Khara <kevalkhara95@gmail.com>
2018-08-31 00:57:35 -0400

    Triangular, pentagonal and hexagonal

--- problem045.py
+++ problem045.py
@@ -1,1 +1,10 @@
 # Copyright 2018 Keval Khara kevalk@bu.edu
+
+T = set((n * (n + 1) / 2) for n in range(2, 200000))
+P = set((n * ((3 * n) - 1) / 2) for n in range(2, 200000))
+H = set((n * ((2 * n) - 1)) for n in range(2, 200000))
+for i in T:
+    if i in P and i in H:
+        print(i)
+    else:
+        continue
