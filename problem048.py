04abb83137d047cdc92dee614a9d465b15d2505a
Keval Khara <kevalkhara95@gmail.com>
2018-11-18 15:26:16 -0500

    Self powers

--- problem048.py
+++ problem048.py
@@ -1,5 +1,7 @@
+# Copyright 2018 Keval Khara kevalk@bu.edu
+
 L = 1000    
 d = 10**10
 res = sum(pow(n, n) for n in range(1, L+1)) 
 
-print(res % d) # because we only want last 10 digits
+print(res % d)   # because we only want last 10 digits
